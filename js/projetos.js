3b69e8d104da6d201a3b2865dea89487a31ee6d8
FerreiraGabrielW <ferreira.gabrielw@gmail.com>
2025-06-12 12:07:11 -0300

    Alterações nas paginas e adição de +1 projeto de Dashboard

--- js/projetos.js
+++ js/projetos.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
             category: 'Data Science', 
             title: 'Classificação de Clientes em Marketing', 
             tools: 'Python, PCA, KNN', 
-            description: 'Descrição completa do projeto 1.', 
+            description: 'O objetivo desse projeto foi utilizar técnicas de aprendizado não supervisionado (clusterização) para identificar grupos distintos de casais com base em suas respostas, permitindo a criação de estratégias de marketing mais eficazes e personalizadas.', 
             link: 'projetos/DataScience/1AnaliseMarketingCasalProspero/AnaliseMarketingCasalProspero.html',
             image: 'projetos/DataScience/1AnaliseMarketingCasalProspero/projetocluster.png'
         },
@@ -22,46 +22,19 @@ document.addEventListener('DOMContentLoaded', function () {
         },
         // Projetos de Dashboards
         { 
-            id: 'dash-proj-1', 
+            id: 'dash-proj-11', 
             category: 'Dashboards', 
-            title: 'RH e Performance de Contratos', 
-            tools: 'Power BI', 
-            description: 'Este projeto foi desenvolvido com o propósito de proporcionar uma visão sobre a composição da força de trabalho de uma organização fictícia e a performance de seus contratos.', 
-            link: 'projetos/Dashboards/1DashboardFuncionarios/Projeto1DA.html',
-            image: 'projetos/Dashboards/1DashboardFuncionarios/capa.png'
+            title: 'Perfil do Mentorado', 
+            tools: 'Power BI, DAX, M', 
+            description: ' Projeto que capicitou a empresa que trabalha com programas de mentoria a compreender melhor seu público-alvo, personalizar as ofertas de capacitação, otimizar a alocação de recursos e identificar talentos com alto potencial para desenvolvimento e migração de área.', 
+            link: 'projetos/Dashboards/11ProjetoPerfilMentorado/Projeto11DA.html',
+            image: 'projetos/Dashboards/11ProjetoPerfilMentorado/capa.png'
         },
-        { 
-            id: 'dash-proj-2', 
-            category: 'Dashboards', 
-            title: 'Auto Solutions', 
-            tools: 'Power BI', 
-            description: 'Este projeto consiste na criação de um Dashboard desenvolvido no Power BI, com o objetivo de oferecer uma visão abrangente e detalhada sobre a performance das operações', 
-            link: 'projetos/Dashboards/2DashboardAutoSolutions/Projeto2DA.html',
-            image: 'projetos/Dashboards/2DashboardAutoSolutions/capa.png'
-        },
-        { 
-            id: 'dash-proj-3', 
-            category: 'Dashboards', 
-            title: 'Bite Speed', 
-            tools: 'Power BI', 
-            description: 'Este projeto apresenta um Dashboard desenvolvido para a empresa fictícia BiteSpeed, com o objetivo primordial de fornecer uma visão consolidada e acionável sobre as operações.', 
-            link: 'projetos/Dashboards/3DashboardBiteSpeed/Projeto3DA.html',
-            image: 'projetos/Dashboards/3DashboardBiteSpeed/capa.png'
-        },
-        { 
-            id: 'dash-proj-4', 
-            category: 'Dashboards', 
-            title: 'Tech Trend', 
-            tools: 'Power BI', 
-            description: 'Este projeto apresenta um Dashboard de Marketing Analytics, construído para fornecer uma visão abrangente do desempenho das campanhas digitais e do comportamento do usuário', 
-            link: 'projetos/Dashboards/4CaseTechTrends/Projeto4DA.html',
-            image: 'projetos/Dashboards/4CaseTechTrends/capa.png'
-        },
-        { 
+            { 
             id: 'dash-proj-5', 
             category: 'Dashboards', 
             title: 'Projeto Piracanjuba', 
-            tools: 'Power BI', 
+            tools: 'Power BI, DAX, M',   
             description: 'Este é um projeto real, e consiste na criação de um dashboard desenvolvido para a Piracanjuba, com o objetivo principal de avaliar o engajamento e o desenvolvimento dos colaboradores que estão participando de um curso', 
             link: 'projetos/Dashboards/5ProjetoPiracanjuba/Projeto5DA.html',
             image: 'projetos/Dashboards/5ProjetoPiracanjuba/capa.png'
@@ -70,16 +43,52 @@ document.addEventListener('DOMContentLoaded', function () {
             id: 'dash-proj-6', 
             category: 'Dashboards', 
             title: 'Amazon Sales', 
-            tools: 'Power BI', 
+            tools: 'Power BI, DAX, M',  
             description: 'Este projeto apresenta um Dashboard de análise de vendas e lucratividade, desenvolvido para simular o ambiente de e-commerce e varejo da Amazon', 
             link: 'projetos/Dashboards/6DadosAmazonSales/Projeto6DA.html',
             image: 'projetos/Dashboards/6DadosAmazonSales/capa.png'
         },
         { 
+            id: 'dash-proj-4', 
+            category: 'Dashboards', 
+            title: 'Tech Trend', 
+            tools: 'Power BI, DAX, M',   
+            description: 'Este projeto apresenta um Dashboard de Marketing Analytics, construído para fornecer uma visão abrangente do desempenho das campanhas digitais e do comportamento do usuário', 
+            link: 'projetos/Dashboards/4CaseTechTrends/Projeto4DA.html',
+            image: 'projetos/Dashboards/4CaseTechTrends/capa.png'
+        },
+        { 
+            id: 'dash-proj-1', 
+            category: 'Dashboards', 
+            title: 'RH e Performance de Contratos', 
+            tools: 'Power BI, DAX, M',  
+            description: 'Este projeto foi desenvolvido com o propósito de proporcionar uma visão sobre a composição da força de trabalho de uma organização fictícia e a performance de seus contratos.', 
+            link: 'projetos/Dashboards/1DashboardFuncionarios/Projeto1DA.html',
+            image: 'projetos/Dashboards/1DashboardFuncionarios/capa.png'
+        },
+        { 
+            id: 'dash-proj-2', 
+            category: 'Dashboards', 
+            title: 'Auto Solutions', 
+            tools: 'Power BI, DAX, M',  
+            description: 'Este projeto consiste na criação de um Dashboard desenvolvido no Power BI, com o objetivo de oferecer uma visão abrangente e detalhada sobre a performance das operações', 
+            link: 'projetos/Dashboards/2DashboardAutoSolutions/Projeto2DA.html',
+            image: 'projetos/Dashboards/2DashboardAutoSolutions/capa.png'
+        },
+        { 
+            id: 'dash-proj-3', 
+            category: 'Dashboards', 
+            title: 'Bite Speed', 
+            tools: 'Power BI, DAX, M',   
+            description: 'Este projeto apresenta um Dashboard desenvolvido para a empresa fictícia BiteSpeed, com o objetivo primordial de fornecer uma visão consolidada e acionável sobre as operações.', 
+            link: 'projetos/Dashboards/3DashboardBiteSpeed/Projeto3DA.html',
+            image: 'projetos/Dashboards/3DashboardBiteSpeed/capa.png'
+        },
+        { 
             id: 'dash-proj-7', 
             category: 'Dashboards', 
             title: 'Campeonato Brasileiro', 
-            tools: 'Power BI', 
+            tools: 'Power BI, DAX, M',  
             description: 'Este projeto apresenta um Dashboard analítico com foco nas estatísticas do Campeonato Brasileiro de Futebol.', 
             link: 'projetos/Dashboards/7DadosCampeonatoBrasileiro/Projeto7DA.html',
             image: 'projetos/Dashboards/7DadosCampeonatoBrasileiro/capa.png'
@@ -88,7 +97,7 @@ document.addEventListener('DOMContentLoaded', function () {
             id: 'dash-proj-8', 
             category: 'Dashboards', 
             title: 'Spotify', 
-            tools: 'Power BI, Python, HTML', 
+            tools: 'Power BI, DAX, M, Python, HTML',  
             description: 'Este projeto apresenta um Dashboard analítico do Spotify, focado em explorar as tendências musicais, o engajamento da audiência e as características das músicas mais populares.', 
             link: 'projetos/Dashboards/8DashboardSpotify/Projeto8DA.html',
             image: 'projetos/Dashboards/8DashboardSpotify/capa.png'
